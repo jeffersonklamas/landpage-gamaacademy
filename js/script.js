f01652deec73a04b40587ce6b646a92af7b560dc
jeffersonklamas <jeffersonklamas@gmail.com>
2021-07-15 20:20:23 -0300

    Incluindo contador de dias, e corringindo CSS.

--- js/script.js
+++ js/script.js
@@ -16,14 +16,43 @@ form.addEventListener('submit', (e) => {
     alert('Enviado!!!')
 
     // let content = document.getElementById('content')
-
     // let levando = `<p> Salvando em nossos arquivos</p>`;
-
     // let finalizado = `<p>Agora, esta bem seguro</p>`
-
     // content.innerHTML = levando;
-
     // setTimeout(() => {
     //     content.innerHTML = finalizado;
     // }, 1000)
-})
+ })
+ 
+ function countdown(){
+    let now = new Date();
+                // Altere a data do seu evento aqui
+    let eventDate = new Date("nov 24, 2021 23:59:00");
+    let currentTiime = now.getTime();
+    let eventTime = eventDate.getTime();
+    let remTime = eventTime - currentTiime;
+    
+    // calculando o dia, hora, minuto e segundo
+    let d = Math.floor(remTime / (1000 * 60 * 60 * 24));
+    let h = Math.floor((remTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let m = Math.floor((remTime % (1000 * 60 * 60)) / (1000 * 60));
+    let s = Math.floor((remTime % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = d;
+    document.getElementById("days").innerText = d;
+    document.getElementById("hours").textContent = h;
+    document.getElementById("minutes").textContent = m;
+    document.getElementById("seconds").textContent = s;
+    setTimeout(countdown, 1000);
+
+    // Verifica se acabou o período do evento
+     if (remTime < 0) {
+      clearInterval(countdown);
+      //document.getElementById("demogrande").innerHTML = "FIM";
+      document.getElementById("days").innerHTML = " ";
+      document.getElementById("hours").innerHTML = " ";
+      document.getElementById("minutes").innerHTML = " ";
+      document.getElementById("seconds").innerHTML = "<small>FIM</small>"; 
+      } 
+  }
+  countdown();
